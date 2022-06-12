424035ae8ba0c2470fbe34a321bdbb6ab1ffeeb9
Horatiu Roman <horatiuromantic@gmail.com>
2022-06-12 12:04:32 +0200

    hosue roof switfher

---
+++ Assets/TiltyFloor/HouseRoofGame/HouseSwitcher.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using ATVR;
+using Normal.Realtime;
+using ToyBoxHHH;
+using UnityEngine;
+
+public class HouseSwitcher : MonoBehaviour
+{
+    public List<Transform> places = new List<Transform>();
+    private int placeIndex;
+    
+    private void Update()
+    {
+        if (VRInput.Get(Hand.Primary).GetPressDown(Button.ButtonOne))
+        {
+            GoToNextPlace();
+        }
+    }
+
+    [DebugButton]
+    private void GoToNextPlace()
+    {
+        placeIndex++;
+        placeIndex = placeIndex % places.Count;
+        transform.GetComponent<RealtimeTransform>().RequestOwnership();
+        transform.position = places[placeIndex].position;
+        transform.rotation = places[placeIndex].rotation;
+        transform.localScale = places[placeIndex].localScale;
+    }
+}
